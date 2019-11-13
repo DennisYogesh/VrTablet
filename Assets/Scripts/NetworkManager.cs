b75d60ec1106d6ef240419f9f6f712c2ff6abfdc
DennisYogesh <DennisYogesh@github.com>
2019-11-13 17:56:03 +0100

    Erster Commit
    Uploade mit drap und drop die Daten, sollte aber funktionieren das man über Unity das öffnen kann.
    
    Unity Version: 2019.2.7f2

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkManager : MonoBehaviour
+{
+    GameObject mainMenu; 
+
+    // Start is called before the first frame update
+
+    //Methode um Serververbindung aufzubauen
+    public void Connect()
+    {
+        Debug.Log("Connect wird ausgeführt!");
+        //Befehl für die Verbindung zum Photon-Network
+        PhotonNetwork.ConnectUsingSettings("v1");
+    }
+
+    private void OnConnectedToMaster()
+    {
+        Debug.Log("Mit Master verbunden. Szene für Lobby laden.");
+        PhotonNetwork.JoinLobby();
+    }
+
+    void OnJoinedLobby()
+    {
+        Debug.Log("Mit Lobby verbunden");
+        mainMenu.SetActive(false);
+        //Zufälligen Raum betreten
+        PhotonNetwork.JoinRandomRoom();
+    }
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        //Spawn ausführen
+        Spawn();
+    }
+
+    void Spawn()
+    {
+        int randomX = Random.Range(-8, +8);
+        int randomY = Random.Range(-4, +4);
+        //Erscheinen des Prefabs
+        PhotonNetwork.Instantiate("Player", new Vector3(randomX, randomY, 0), Quaternion.identity, 0);
+        //PhotonNetwork.Instantiate("Player", new Vector3(0, 0, 0), Quaternion.identity, 0);
+
+    }
+    void Start()
+    {
+        //Ausgabe von aktuellem Serverstatus
+        mainMenu = GameObject.Find("MainMenu");
+        //Áusgabe Anzahl weiterer Spieler im Raum
+        Debug.Log("Anzahl weiterer Spieler " + PhotonNetwork.otherPlayers.Length);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Ausgabe des aktuellen Serverstatus
+        Debug.Log(PhotonNetwork.connectionStateDetailed.ToString());
+
+        //Ausgabe Anzahl weiterer Spieler im Raum
+        Debug.Log("Anzahl weiterer Spieler: " + PhotonNetwork.otherPlayers.Length);
+    }
+}
---
+++ Assets/Scripts/NetworkPlayer.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NetworkPlayer : Photon.MonoBehaviour
+{
+    private Vector3 correctPlayerPos;
+    private Quaternion correctPlayerRot;  
+    public GameObject myCamera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Kamera aktivieren
+        myCamera.SetActive(true);
+        //Steuerung aktivieren
+        GetComponent<PlayerMovement>().enabled = true;
+        
+    }
+
+    void onPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.isWriting)
+        {
+            //We own this player: send the others our data
+            stream.SendNext(transform.position);
+            stream.SendNext(transform.rotation);
+        }
+        else
+        {
+            //Network player, receive data
+            this.correctPlayerPos = (Vector3)stream.ReceiveNext();
+            this.correctPlayerRot = (Quaternion)stream.ReceiveNext();
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!photonView.isMine)
+        {
+            /*
+            //Kamera aktivieren
+            myCamera.SetActive(true);
+            //Steuerung aktivieren
+            GetComponent<PlayerMovement>().enabled = true;
+            */
+
+            transform.position = Vector3.Lerp(transform.position, this.correctPlayerPos, Time.deltaTime * 5);
+            transform.rotation = Quaternion.Lerp(transform.rotation, this.correctPlayerRot, Time.deltaTime * 5);
+        }
+
+    }
+}
